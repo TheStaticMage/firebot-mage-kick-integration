c825e548d1fe41423150c4191d245a34d2390d56
The Static Mage <199814801+TheStaticMage@users.noreply.github.com>
2025-09-04 20:40:14 -0500

    Fix timeout validation in timeout effect (#79)

--- src/effects/moderator-timeout.ts
+++ src/effects/moderator-timeout.ts
@@ -5,7 +5,7 @@ import { IntegrationConstants } from "../constants";
 
 export const moderatorTimeoutEffect: Firebot.EffectType<{
     username: string;
-    time: number;
+    time: string | number;
     reason: string;
 }> = {
     definition: {
@@ -24,7 +24,7 @@ export const moderatorTimeoutEffect: Firebot.EffectType<{
             </div>
         </eos-container>
         <eos-container header="Time" pad-top="true">
-            <p class="muted">Due to API limitations, the minimum timeout duration is 1 minute, and the time you specify here will be rounded to the nearest minute.</p>
+            <p class="muted">Due to API limitations, the permitted range is 60-604800 seconds (1 minute-7 days). The seconds you specify here will be rounded to the nearest minute.</p>
             <div class="input-group">
                 <span class="input-group-addon" id="time-type">Time (Seconds)</span>
                 <input ng-model="effect.time" type="text" class="form-control" id="list-username-setting" aria-describedby="list-time-type" placeholder="Seconds" replace-variables="number">
@@ -46,7 +46,9 @@ export const moderatorTimeoutEffect: Firebot.EffectType<{
         if (effect.username == null || effect.username === "") {
             errors.push("Please enter a username.");
         }
-        if (effect.time == null) {
+        if (effect.time == null || effect.time === "") {
+            // We can't check for a number here or do numeric comparisons
+            // because the value could be a replace variable.
             errors.push("Please enter an amount of time.");
         }
         return errors;
@@ -55,17 +57,21 @@ export const moderatorTimeoutEffect: Firebot.EffectType<{
         return `${effect.time} sec timeout for ${effect.username}`;
     },
     onTriggerEvent: async (event) => {
-        const duration = Math.round(event.effect.time / 60);
-        if (duration < 1 || duration > 10080) {
-            logger.error(`Invalid timeout duration: ${duration}`);
+        // Here the time should be a number since all variables should have been
+        // resolved.
+        const timeAsNumber = Number(event.effect.time);
+        if (isNaN(timeAsNumber)) {
+            logger.error(`Invalid timeout time provided: ${event.effect.time}`);
             return false;
         }
 
+        // Range to the Kick API is 1-10080 minutes (7 days)
+        const duration = Math.max(1, Math.min(10080, Math.round(timeAsNumber / 60)));
         const isTimedOut = await integration.kick.userApi.banUserByUsername(event.effect.username, duration, true, event.effect.reason || "Timed out via Firebot");
         if (isTimedOut) {
-            logger.debug(`${event.effect.username} was timed out via the Timeout effect.`);
+            logger.debug(`${event.effect.username} was timed out via the Timeout effect (duration=${duration} minutes).`);
         } else {
-            logger.error(`${event.effect.username} was unable to be timed out via the Timeout effect.`);
+            logger.error(`${event.effect.username} was unable to be timed out via the Timeout effect (duration=${duration} minutes).`);
             return false;
         }
         return true;
