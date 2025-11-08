8ded3f893842a21943d45362b431250391f00fdc
The Static Mage <199814801+TheStaticMage@users.noreply.github.com>
2025-11-08 12:21:02 -0600

    Update scriptDataDir for 5.65

--- src/util/datafile.ts
+++ src/util/datafile.ts
@@ -1,29 +1,11 @@
-import { IntegrationConstants } from "../constants";
 import { firebot } from "../main";
-declare const SCRIPTS_DIR: string; // Old method for compatibility
 
 export function getDataFilePath(filename: string): string {
     const { fs, path } = firebot.modules;
-    const filepath = `script-data/${IntegrationConstants.INTEGRATION_URI}/${filename}`; // Old path for compatibility
-    let result = "";
-
-    try {
-        // Requires a version of Firebot that exposes the script directory.
-        // See https://github.com/crowbartools/Firebot/issues/3180
-        const { path, scriptDataDir } = firebot.modules;
-        result = path.join(scriptDataDir, filename);
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    } catch (error) {
-        // Fall back to the legacy method, compatible with older versions of Firebot.
-        const profileDirectory = path.join(SCRIPTS_DIR, '..');
-        const pathSplit = filepath.split('/');
-        result = path.join(profileDirectory, ...pathSplit);
+    const { scriptDataDir } = firebot;
+    if (!fs.existsSync(scriptDataDir)) {
+        fs.mkdirSync(scriptDataDir, { recursive: true });
     }
 
-    const dir = path.dirname(result);
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir, { recursive: true });
-    }
-
-    return result;
+    return path.join(scriptDataDir, filename);
 }
